f40b1c94fe5468e8a8278f2bdecc16926884d564
tkogiah <87034470+Tkogiah@users.noreply.github.com>
2021-11-27 22:50:10 -0800

    more style changes

---
+++ index.js
@@ -0,0 +1,13 @@
+//use zip code to pull data
+//use data to communicate
+
+
+// let xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        document.getElementById("demo").innerHTML = xhttp.responseText;
+//     }
+// };
+// xhttp.open("GET", "filename", true);
+// xhttp.send();
